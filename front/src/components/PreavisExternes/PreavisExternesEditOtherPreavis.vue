289d97cbcd30a6655f2bfd0ef91656e173fd32bf
Marc Rufener <marc.rufener@gmail.com>
2025-10-15 14:41:19 +0200

    add preavis icon status

--- front/src/components/PreavisExternes/PreavisExternesEditOtherPreavis.vue
+++ front/src/components/PreavisExternes/PreavisExternesEditOtherPreavis.vue
@@ -30,6 +30,23 @@ export default {
       ).then(response => {
         if (response && response.data) {
           this.preavisList = response.data.filter(x => x.id !== this.preavis_id);
+
+          // add icon status
+          this.preavisList.forEach(x => {
+            if (x.preavis_type_id === 1) {
+              x.icon_status = 'check_circle_outline';
+              x.icon_status_style = 'color: green;';
+            } else if (x.preavis_type_id === 2) {
+              x.icon_status = 'highlight_off';
+              x.icon_status_style = 'color: red;';
+            } else if (x.preavis_type_id === 3) {
+              x.icon_status = 'remove_circle_outline';
+              x.icon_status_style = 'color: grey;';
+            } else if (x.preavis_type_id === 5) {
+              x.icon_status = 'error_outline';
+              x.icon_status_style = 'color: orange;';
+            }
+          });
         }
       }).catch(err => handleException(err));
     }
